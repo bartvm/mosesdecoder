3663d459011ee0742e15179d3e89bbeafe64066f
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2014-04-20 10:43:00 -0400

    Formatting

--- cslm.py
+++ cslm.py
@@ -12,8 +12,8 @@ logging.basicConfig(filename='cslm.log', level=logging.DEBUG)
 sys.stdout = open('cslm.log', 'a')
 sys.stderr = open('cslm.log', 'a')
 
-profmode = theano.ProfileMode(optimizer='fast_run',
-                              linker=theano.gof.OpWiseCLinker())
+profmode = ProfileMode(optimizer='fast_run',
+                       linker=theano.gof.OpWiseCLinker())
 
 logging.debug("Imported modules")
 
@@ -37,19 +37,20 @@ f = theano.function(
 
 logging.debug("Compiled Theano function")
 
+
 def filter(ngrams):
-    inputs, targets = numpy.ascontiguousarray(ngrams[:, :-1]), numpy.ascontiguousarray(ngrams[:, -1])
+    inputs, targets = ngrams[:, :-1], ngrams[:, -1]
     sorted_indices = numpy.lexsort(inputs.T[::-1])
     reverse_sorted_indices = numpy.argsort(sorted_indices)
     unique_indices = numpy.nonzero(numpy.concatenate((
-        [True], # Always take the first row
+        [True],  # Always take the first row
         numpy.any(inputs[sorted_indices[1:]] != inputs[sorted_indices[:-1]],
-                  axis=1) # Only the unique ones
+                  axis=1)  # Only the unique ones
     )))[0]
     final_inputs = inputs[sorted_indices][unique_indices]
     repeats = numpy.roll(unique_indices, -1) - unique_indices
     repeats[-1] += inputs.shape[0]
-    
+
     target_samples = numpy.repeat(numpy.arange(unique_indices.shape[0],
                                                dtype='int32'), repeats)
     target_words = targets[sorted_indices]
@@ -65,6 +66,7 @@ def get(ngrams, scores, batch_size):
 
 lprof = line_profiler.LineProfiler(filter, get)
 lprof.enable()
+
 
 def profile():
     lprof.disable()
