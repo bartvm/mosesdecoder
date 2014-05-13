4572a0504f52433d47823334f0b3d3f847a28c7c
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2014-05-13 11:03:10 -0400

    More cleanup

--- cslm.py
+++ cslm.py
@@ -1,26 +1,29 @@
 import cPickle
 import logging
-import sys
+import signal
 
 import numpy
 import theano
 
-from theano import ProfileMode
-import line_profiler
+logging.basicConfig(level=logging.DEBUG)
 
-logging.basicConfig(filename='cslm.log', level=logging.DEBUG)
-sys.stdout = open('cslm.log', 'a')
-sys.stderr = open('cslm.log', 'a')
 
-profmode = ProfileMode(optimizer='fast_run',
-                       linker=theano.gof.OpWiseCLinker())
+def handler(signum, frame):
+    """
+    We catch the SIGINT that is broadcast when Moses is quit using
+    e.g. CTRL + C, otherwise an uncaught KeyboardInterrupt is raised,
+    causing the embedded interpreter to segfault.
+    """
+    pass
 
-logging.debug("Imported modules")
+signal.signal(signal.SIGINT, handler)
 
-with open('/home/vanmerb/mosesdecoder/europarl_cslm.pkl') as f:
+logging.debug("Loading model...")
+
+with open('/u/vanmerb/pylearn2/holger_best.pkl') as f:
     model = cPickle.load(f)
 
-logging.debug("Loaded model")
+logging.debug("Compiling Theano function...")
 
 input = theano.tensor.imatrix()
 windows = theano.tensor.ivector()
@@ -30,13 +33,37 @@ f = theano.function(
     [input, windows, targets],
     theano.tensor.log10(
         results.flatten()[windows * results.shape[1] + targets]
-    ), mode=profmode
+    )  # , mode=profmode
 )
 
-logging.debug("Compiled Theano function")
+logging.debug("Python module loaded!")
 
 
 def filter(ngrams):
+    """
+    This function takes a matrix of ngrams and filters
+    out all the duplicate contexts (i.e. n-1 inputs).
+
+    Parameters
+    ----------
+    ngrams : 2d ndarray
+        An array with rows of n-grams
+
+    Returns
+    -------
+    final_inputs : 2d ndarray
+        An array of unique n-1 grams (contexts)
+    target_samples : 1d ndarray
+        An array of indices to final_inputs, with
+        duplicate entries for when multiple targets
+        are needed from a single input
+    target_words : 1d ndarray
+        An array of targets, corresponding to the
+        samples given in target_samples
+    reverse_sorted_indices : 1d ndarray
+        The indices required to reorder the target
+        words back to the original input
+    """
     inputs, targets = ngrams[:, :-1], ngrams[:, -1]
     sorted_indices = numpy.lexsort(inputs.T[::-1])
     reverse_sorted_indices = numpy.argsort(sorted_indices)
@@ -56,21 +83,28 @@ def filter(ngrams):
 
 
 def get(ngrams, scores, batch_size):
+    """
+    Scores a given number of ngrams and writes the scores
+    to a given vector.
+
+    Parameters
+    ----------
+    ngrams : 2d ndarray
+        A matrix of ngrams
+    scores : 1d ndarray
+        A vector of scores
+    batch_size : int
+        Only the first batch_size rows will
+        be scored, and the scores will be written
+        to the first batch_size elements of the scores
+        vector.
+
+    Returns
+    -------
+    True if successful
+    """
     (final_inputs, target_samples,
         target_words, reverse_sorted_indices) = filter(ngrams[:batch_size])
     results = f(final_inputs, target_samples, target_words)
     scores[:batch_size] = results[reverse_sorted_indices]
     return True
-
-lprof = line_profiler.LineProfiler(filter, get)
-lprof.enable()
-
-
-def profile():
-    lprof.disable()
-    lprof.print_stats()
-    profmode.print_summary()
-    sys.stdout.flush()
-
-import atexit
-atexit.register(profile)
